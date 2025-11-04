610e8758407885e2d819ee5741045da068f823ce
Surapon NONESUNG <surapon@fc4f4ac1-02.cloud.together.ai>
2025-11-03 22:59:45 -0800

    first commit

---
+++ eval_scripts/page_ocr_metric.py
@@ -0,0 +1,60 @@
+import json
+import argparse
+import nltk
+from nltk.metrics import precision, recall, f_measure
+import numpy as np
+import jieba
+import re
+from nltk.translate import meteor_score
+from pythainlp.tokenize import word_tokenize
+
+def contain_chinese_string(text):
+    chinese_pattern = re.compile(r'[\u4e00-\u9fa5]')
+    return bool(chinese_pattern.search(text))
+
+def contain_thai_string(text):
+    thai_pattern = re.compile(r'[\u0E00-\u0E7F]')
+    return bool(thai_pattern.search(text))
+
+def cal_per_metrics(pred, gt):
+    metrics = {}
+
+    
+    if contain_thai_string(gt) or contain_thai_string(pred):
+        # reference = gt.split()
+        # hypothesis = pred.split()
+        reference = word_tokenize(gt, engine='newmm')
+        hypothesis = word_tokenize(pred, engine='newmm')
+    elif contain_chinese_string(gt) or contain_chinese_string(pred):
+        reference = jieba.lcut(gt)
+        hypothesis = jieba.lcut(pred)
+    else:
+        reference = gt.split()
+        hypothesis = pred.split()
+
+    metrics["bleu"] = nltk.translate.bleu([reference], hypothesis)
+    metrics["meteor"] = meteor_score.meteor_score([reference], hypothesis)
+
+    reference = set(reference)
+    hypothesis = set(hypothesis)
+    metrics["f_measure"] = f_measure(reference, hypothesis)
+
+    metrics["precision"] = precision(reference, hypothesis)
+    metrics["recall"] = recall(reference, hypothesis)
+    metrics["edit_dist"] = nltk.edit_distance(pred, gt) / max(len(pred), len(gt))
+    return metrics
+
+
+if __name__ == "__main__":
+
+    # Examples for region text recognition and read all text tasks
+    predict_text = "metrics['edit_dist'] = nltk.edit_distance(pred, gt) / max(len(pred), len(gt))"
+    true_text = "metrics = nltk.edit_distance(pred, gt) / max(len(pred), len(gt))"
+
+    scores = cal_per_metrics(predict_text, true_text)
+
+    predict_text = "metrics['edit_dist'] len(gt))"
+    true_text = "metrics = nltk.edit_distance(pred, gt) / max(len(pred), len(gt))"
+
+    scores = cal_per_metrics(predict_text, true_text)
+    print(scores)
